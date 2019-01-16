f57164a9c718f973c64cb12be49406fd29e99eb2
Dennis Foster <dennis@appkillerz.com>
2019-01-16 13:16:01 -0900

    Added users name and email to users db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_16_082009) do
+ActiveRecord::Schema.define(version: 2019_01_16_220852) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,9 @@ ActiveRecord::Schema.define(version: 2019_01_16_082009) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email_address"
   end
 
 end
