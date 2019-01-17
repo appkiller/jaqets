a54a6a09788ec24ebc7bd870edd5382f07a4eb24
Dennis Foster <dennis@appkillerz.com>
2019-01-16 15:37:32 -0900

    Added user login and authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,16 @@
 class User < ApplicationRecord
-  validates :username, presence: true, length: { minimum: 4, maximum: 15 }
+  before_save { self.email_address = email_address.downcase }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email_address,
+            presence: true,
+            uniqueness: { case_sensitive: false },
+            length: { maximum: 105 },
+            format: { with: VALID_EMAIL_REGEX }
+  validates :first_name,
+            presence: true,
+            length: { maximum: 24 }
+  validates :last_name,
+            presence: true,
+            length: { maximum: 24 }
   has_secure_password
 end
